55103c39e8e6a5db8cc8c0b01b252cc44b78852d
nlasvn <114028830+nlasvn@users.noreply.github.com>
2025-02-01 22:59:55 -0300

    refactor to use grid layout and oop

--- Desktop/Cadastro_de_plantas/cadastrar_planta.py
+++ Desktop/Cadastro_de_plantas/cadastrar_planta.py
@@ -1,50 +1,63 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QFormLayout, QLineEdit
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QPushButton,
+    QLineEdit,
+    QSizePolicy,
+    QGridLayout,
+)
+
+
+class CadastroPlantasWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Cadastro de plantas")
+        self.setGeometry(50, 200, 350, 200)
+        self.setFixedSize(350, 200)
+
+        self.inicializar_interface()
+
+    def inicializar_interface(self):
+        self.header_label = QLabel("Faça o Cadastro")
+        self.header_label.setSizePolicy(
+            QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed
+        )
+
+        self.cadastro_label = QLabel("Cadastrar Nova Planta")
+
+        self.nome_popular_edit = QLineEdit()
+        self.nome_cientifico_edit = QLineEdit()
+
+        self.nome_popular_edit.setPlaceholderText("Nome Popular")
+        self.nome_cientifico_edit.setPlaceholderText("Nome Científico")
+
+        self.inserir_imagem_label = QLabel("Inserir Imagem")
+        self.inserir_imagem_button = QPushButton("Upload")
+
+        self.cancelar_button = QPushButton("Cancelar")
+        self.cadastrar_button = QPushButton("Cadastrar")
+
+        self.layout = QGridLayout(self)
+        self.layout.setSpacing(5)
+        self.layout.setContentsMargins(10, 10, 10, 10)
+
+        self.layout.addWidget(self.header_label, 0, 0, 1, 2)
+        self.layout.addWidget(self.cadastro_label, 1, 0, 1, 2)
+        self.layout.addWidget(self.nome_popular_edit, 2, 0, 1, 2)
+        self.layout.addWidget(self.nome_cientifico_edit, 3, 0, 1, 2)
+        self.layout.addWidget(self.inserir_imagem_label, 4, 0)
+        self.layout.addWidget(self.inserir_imagem_button, 4, 1)
+        self.layout.addWidget(self.cancelar_button, 5, 0)
+        self.layout.addWidget(self.cadastrar_button, 5, 1)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    window = QWidget()
-    window.setWindowTitle("Cadastro de plantas")
-    window.setGeometry(50, 200, 350, 150)
-
-    header_label = QLabel("Faça o Cadastro", window)
-    header_label.move(100, 30)
-
-    cadastro_label = QLabel("Cadastrar Nova Planta", window)
-
-    nome_popular_edit = QLineEdit()
-    nome_cientifico_edit = QLineEdit()
-
-    nome_popular_edit.setPlaceholderText("Nome Popular")
-    nome_cientifico_edit.setPlaceholderText("Nome Científico")
-
-    inserir_imagem_label = QLabel("Inserir Imagem", window)
-    inserir_imagem_button = QPushButton("Upload", window)
-    
-    cancelar_button = QPushButton("Cancelar")
-    cadastrar_button = QPushButton("Cadastrar")
-
-
-    layout = QVBoxLayout(window)
-    layout.addWidget(header_label)
-    layout.addWidget(cadastro_label)
-    layout.addWidget(nome_popular_edit)
-    layout.addWidget(nome_cientifico_edit)
-    layout.addWidget(inserir_imagem_label)
-    layout.addWidget(inserir_imagem_button)
-    layout.addWidget(cancelar_button)
-    layout.addWidget(cadastrar_button)
-
-
-    # cadastro_layout = QFormLayout(window)
-    # cadastro_layout.addRow(QLabel("Nome Popular"), nome_popular_edit)
-    # cadastro_layout.addRow(QLabel("Nome Científico"), nome_cientifico_edit)
-    
-
-
-    # layout = QVBoxLayout(window)
-    # layout.addWidget(label_text)
-
+    window = CadastroPlantasWindow()
     window.show()
-    sys.exit(app.exec_())
+
+    sys.exit(app.exec())
